b5e0a81dcbe3e324a23675f466593565bf777db7
evwilkin <evan.wilkinson@gmail.com>
2016-02-05 17:44:43 -0800

    Completed assignment oop-inheritance-car

--- Motorcycle.js
+++ Motorcycle.js
@@ -1,6 +1,20 @@
 var Car = require('./Car.js');
 
 //Implement your Motorcycle here
-function Motorcycle() {};
+function Motorcycle(make, model, year, color, passengers) {
+	Car.call(this, make, model, year, color, 2, passengers);
+	this.wheelie = function() {
+		var popWheelie = false;
+		if (this.running) {
+			popWheelie = true;
+			console.log("Doing a sick wheelie!!");
+		}
+		return popWheelie;
+	}
+};
+Motorcycle.prototype = Object.create(Car.prototype);
+Motorcycle.prototype.constructor = Motorcycle;
+
+var harley = new Motorcycle("Harley", "CoolModel", "2016", "red", 1);
 
 module.exports = Motorcycle;
