7001bf58a66f9adcfc23888f90122403bcedb221
k-buffum <knbuffum@gmail.com>
2016-02-07 18:01:37 -0800

    Weekend HW Katlyn Buffum

--- Motorcycle.js
+++ Motorcycle.js
@@ -1,6 +1,31 @@
 var Car = require('./Car.js');
 
 //Implement your Motorcycle here
-function Motorcycle() {};
+function Motorcycle(model, year, color, seats, passengers) {
+	Car.call(this, model, year, color, seats, passengers);
+	this.seats = 2;
+};
 
+Motorcycle.prototype = Object.create(Car.prototype);
+Motorcycle.prototype.constructor = Motorcycle;
+
+Motorcycle.prototype.lessThan2seats = function() {
+	if (this.seats > 2) {
+		return false;
+	} else {
+		return true;
+	}
+};
+
+Motorcycle.prototype.wheelie = function() {
+	if(this.running) {
+		return true;
+		console.log("Doing a sick wheelie!!")
+	} else {
+		return false;
+	}
+};
+
+
+//// LEAVE AT BOTTOM OF PAGE ////
 module.exports = Motorcycle;
--- Truck.js
+++ Truck.js
@@ -1,6 +1,33 @@
 var Car = require('./Car.js');
 
-//Implement your Truck here
-function Truck() {};
+function Truck(make, model, year, color, capacity, cargo) {
+	Car.call(this, model, year, color, 3 , 0);
+	this.make = make;
+	this.capacity = capacity || 0; // Amount of weight truck can carry
+	this.cargo = cargo || 0; // Amount of weight truck is currently carrying
+};
 
+Truck.prototype = Object.create(Car.prototype);
+Truck.prototype.constructor = Truck;
+
+Truck.prototype.load = function(pounds) {
+	if((this.cargo + pounds) < this.capacity) {
+		console.log("loaded " + pounds + " lbs of cargo.");
+		this.cargo = this.cargo + pounds;
+		return true;
+	} else {
+		return false;
+	}
+};
+
+Truck.prototype.unload = function(pounds) {
+	if (pounds <= this.cargo) {
+		this.cargo = this.cargo - pounds;
+		return true;
+	} else {
+		return false;
+	}
+};
+
+//// LEAVE AT BOTTOM OF PAGE ////
 module.exports = Truck;
