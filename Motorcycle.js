076ac99e84e88b317124a901714e91b1078fa17d
cassicakes <aguiar.cas@gmail.com>
2016-02-08 07:04:10 -0800

    oop inheritance homework

--- Motorcycle.js
+++ Motorcycle.js
@@ -1,6 +1,24 @@
 var Car = require('./Car.js');
 
 //Implement your Motorcycle here
-function Motorcycle() {};
+
+// Just a heads up, the HW description was a bit misleading. When you test using `instanceof`, you’ll want to make sure that an `instance` is on the left side, and the `Motorcycle` constructor on the right
+function Motorcycle(make, model, year, color, seats, passengers) {
+	Car.call(this, make, model, year, color, seats, passengers)
+	 //needs a conditional aka if statment:
+	 if (this.seats >= 3){
+	 	this.seats = 2;
+	 };
+	 this.wheelie = function(){
+	 	if (this.running == true) {
+	 		console.log("Doing a sick wheelie");
+	 		return true
+	 	} else {
+	 		return false
+	 	}
+	 }
+};
 
 module.exports = Motorcycle;
+
+//go look for coffee constructor example from class
--- Truck.js
+++ Truck.js
@@ -1,6 +1,35 @@
 var Car = require('./Car.js');
-
 //Implement your Truck here
-function Truck() {};
+function Truck(make, model, year, color, seats, capacity, cargo) {
+	Car.call(this, make, model, year, color, seats);
+	this.capacity = capacity || 0;
+	this.weight = 0;
+	this.cargo = this.weight;
+
+	};
+// load(pounds) should take a weight in pounds and increase the amount of cargo the truck is carrying IF loading the additional weight won't put it over capacity. Should console.log "loaded <pounds>lbs of cargo." and return true if successful and false if the cargo couldn't be loaded (over capacity)
+var load = function(pounds) {
+	if (pounds + weight < capacity) {
+		pounds + weight;
+		console.log("loaded " + pounds +"lbs of cargo");
+		return true
+	} else {
+		return false
+	}
+};	
+
+
+// unload(pounds) should take a weight in pounds and decrease the amount of cargo the truck is carrying and return true. However, if the amount of pounds is greater than the amount of cargo on the truck it should not reduce the cargo and should return false.
+ 
+
+
+ // 	for ( i= 0; i < _______________; i--) {
+ 		
+ // 		} else {
+ // 			pounds > cargo;
+ // 			return false
+ // 		}
+	// };
+
 
 module.exports = Truck;
