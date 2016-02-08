4eccc590fe8a7eb0f995dd15e90922272b265722
Scott <scott.heron@outlook.com>
2016-02-08 10:04:26 -0800

    checking for 2 or less seats on motorcycle, correction

--- Motorcycle.js
+++ Motorcycle.js
@@ -3,7 +3,9 @@ var Car = require('./Car.js');
 //Implement your Motorcycle here
 function Motorcycle(make, model, year, color, seats, passengers) {
 	Car.call(this, make, model, year, color, seats, passengers);
-	
+	if (this.seats > 2){
+		this.seats = 2;
+	}
 
 };
 
@@ -18,6 +20,7 @@ Motorcycle.prototype.wheelie = function(){
 	var result = false;
 	if (this.running) {
 		result = true;
+		console.log("Doing a sick wheelie!")
 	}
 	return result;
 };
