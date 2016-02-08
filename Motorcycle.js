27ddc57442985206785b3f5c885867d3b885111b
aheitzen <alexaheitzenrader@yahoo.com>
2016-02-07 19:00:00 -0800

    inheritance hw

--- Motorcycle.js
+++ Motorcycle.js
@@ -1,6 +1,21 @@
 var Car = require('./Car.js');
 
 //Implement your Motorcycle here
-function Motorcycle() {};
+
+function Motorcycle(make, model, year, color, seats, passengers) {
+	Car.call(this, make, model, year, color, seats, passengers)
+	this.seats = 2;
+	this.wheelie = function() {
+		if(this.running) {
+			console.log("doing a sick wheelie!");
+			return true;
+		} else{
+			return false;
+		}
+
+	};
+};
+
+Motorcycle.prototype = new Car();
 
 module.exports = Motorcycle;
--- Truck.js
+++ Truck.js
@@ -1,6 +1,39 @@
 var Car = require('./Car.js');
 
-//Implement your Truck here
-function Truck() {};
 
+
+//Implement your Truck here
+function Truck(make, model, year, color, seats, passengers, capacity) {
+	var cargo = 0
+	Car.call(this, make, model, year, color, seats, passengers)
+	this.capacity = capacity || 0;
+	this.seats = 3;
+	this.load = function(pounds) {
+		if(cargo + pounds <= this.capacity) {
+			cargo = cargo + pounds;
+			console.log("loaded "+ pounds +" lbs of cargo");
+			return true;
+		} else {
+			return false;
+		}
+	};
+	this.unload = function(pounds) {
+		if(cargo - pounds >= 0){
+			cargo = cargo - pounds;
+			return true;
+		} else {
+			return false;
+		}
+	};
+};
+
+
+
+
+
+
+
+
+
+Truck.prototype = new Car();
 module.exports = Truck;
