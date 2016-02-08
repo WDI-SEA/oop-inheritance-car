604cb686ead19b1c1a9b7da4b5e9d21fd6d2f9f7
katiosity <lovan.kathy@gmail.com>
2016-02-07 23:27:14 -0800

    motorcycle tests complete; working on truck

--- Motorcycle.js
+++ Motorcycle.js
@@ -1,6 +1,43 @@
 var Car = require('./Car.js');
 
 //Implement your Motorcycle here
-function Motorcycle() {};
+
+// Part 1
+
+function Motorcycle(make, model, year, color, seats, passengers) {
+	Car.call(this, make, model, year, color, seats, passengers);
+	this.seats <= 2;
+};
+
+Motorcycle.prototype = Object.create(Car.prototype);
+Motorcycle.prototype.constructor = Motorcycle;
+
+Motorcycle.prototype.wheelie = function() {
+	var result = false;
+	if (!this.running) {
+		result = true;
+		console.log("Doing a stick wheelie!");
+	}
+	return result;
+}
 
 module.exports = Motorcycle;
+
+
+
+
+
+
+
+
+	// Car.call(this, seats);
+	// Car.call(this, running);
+	// this.seats <= 2
+	// this.wheelie = true;
+
+	// var result = false;
+	// if (!this.running) {
+	// 	result = true;
+	// 	console.log("Doing a stick wheelie!!")
+	// }
+	// return result;
--- Truck.js
+++ Truck.js
@@ -1,6 +1,27 @@
 var Car = require('./Car.js');
 
 //Implement your Truck here
-function Truck() {};
+
+function Truck(make, model, year, color, seats, capacity, cargo) {
+	Car.call(this, make, model, year, color);
+	this.seats = 3;
+	this.capacity = capacity || 0;
+	this.cargo = cargo || 0;
+};
+
+Truck.prototype = Object.create(Car.prototype);
+Truck.prototype.constructor = Truck;
+
+Truck.prototype.load = function(pounds) {
+	this.cargo = pounds;
+	if (this.cargo < this.capacity) {
+		return true;
+		pounds++;
+	} else {
+		return false;
+	}
+}
+
+
 
 module.exports = Truck;
