981ac25d3a53b4333d73061247db036102af0dba
Julia Hensley <jh.julia@gmail.com>
2016-03-29 18:29:58 -0700

    Tested motorcycles. I  didn't finish trucks.

--- Truck.js
+++ Truck.js
@@ -1,6 +1,11 @@
-var Car = require('./Car.js');
+var Truck = require('./Truck.js');
 
 //Implement your Truck here
-function Truck() {};
+function Truck(make, model, year, color, seats, passengers) {
+	Car.call(make, model, year, color, seats, passengers)
+};
+
+Truck.prototype = Object.create(Car.prototype);
+Truck.prototype.constructor = Truck;
 
 module.exports = Truck;
