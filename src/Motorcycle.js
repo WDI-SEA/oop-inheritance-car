425845dbdbe3e3c27a386c779b2ac2e3079f746d
Krista Calderon <kamsci@gmail.com>
2016-06-10 16:37:21 -0700

    Completed Exercises

--- src/Motorcycle.js
+++ src/Motorcycle.js
@@ -1,6 +1,20 @@
 var Car = require('./Car.js');
 
 // Implement your Motorcycle here
-function Motorcycle() {}
+function Motorcycle(make, model, year, color, passengers) {
+  Car.call(this, make, model, year, color, 2, passengers);
+}
+
+Car.prototype.wheelie = function(){
+  if (this.running) {
+    console.log("Doing a sick wheelie!!");
+    return true;
+  }
+  return false;
+};
+
+
+Motorcycle.prototype = Object.create(Car.prototype);
+Motorcycle.prototype.constructor = Motorcycle;
 
 module.exports = Motorcycle;
--- test/motorcycleTest.js
+++ test/motorcycleTest.js
@@ -7,3 +7,35 @@ var Motorcycle = require('../src/Motorcycle');
 var Car = require('../src/Car');
 
 // Your tests will go below here
+// test object
+var moto = new Motorcycle("Harley", "T3000", "1995", "red", ["Peggy Hill"]);
+// Tests if motorcycle is an instance of Car and Motercycle
+assert.strictEqual(moto instanceof Motorcycle, true, "moto chould be instance of Moto");
+assert.strictEqual(moto instanceof Car, true, "moto should be instance of Car");
+success();
+
+console.log( "Test properties exist")
+assert.strictEqual(moto.make, "Harley", "moto should have make property");
+success();
+assert.strictEqual(moto.model, "T3000", "moto should have model property");
+success();
+assert.strictEqual(moto.year, "1995", "moto should have year property");
+success();
+assert.strictEqual(moto.color, "red", "moto should have color property");
+success();
+assert.strictEqual(moto.seats, 2, "moto should have seats property");
+success();
+assert.deepEqual(moto.passengers, ["Peggy Hill"], "moto should have passengers property");
+success();
+
+// Car.prototype test
+// Call function
+moto.start();
+console.log(moto);
+console.log(moto.wheelie());
+//
+console.log("Check if wheelie function is a function");
+assert.strictEqual(typeof(moto.wheelie), "function", "wheelie is not a function of moto");
+console.log("Check if wheelie moto is running");
+assert.strictEqual(moto.running, true, "moto is not running");
+success();
