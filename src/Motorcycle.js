2ac07c7e029a2d331d3642cbf8b85927d0689950
Ivar Lee <ivarr5@gmail.com>
2016-06-10 16:24:38 -0700

    making tests to check for props

--- src/Motorcycle.js
+++ src/Motorcycle.js
@@ -1,6 +1,23 @@
 var Car = require('./Car.js');
 
 // Implement your Motorcycle here
-function Motorcycle() {}
+function Motorcycle(make, model, year, color, seats, passengers) {
+  Car.call(this, make, model, year, color, seats, passengers);
+}
+Motorcycle.prototype.start = function() {
+  this.start = true;
+};
+
+Motorcycle.prototype = Object.create(Car.prototype);
+Motorcycle.prototype.constructor = Motorcycle;
+
+Motorcycle.prototype.wheelie = function() {
+  if (this.running) {
+    console.log('Doing a wheelie that is sick');
+  } else {
+    console.log('You should be running');
+  }
+};
+
 
 module.exports = Motorcycle;
