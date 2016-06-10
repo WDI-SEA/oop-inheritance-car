42d20213ada786441627555a57a2a0ebd0551c88
Krista Calderon <kamsci@gmail.com>
2016-06-10 16:48:37 -0700

    Updated wheelie tests

--- src/Motorcycle.js
+++ src/Motorcycle.js
@@ -5,9 +5,9 @@ function Motorcycle(make, model, year, color, passengers) {
   Car.call(this, make, model, year, color, 2, passengers);
 }
 
-Car.prototype.wheelie = function(){
+Car.prototype.wheelie = function() {
   if (this.running) {
-    console.log("Doing a sick wheelie!!");
+    console.log('Doing a sick wheelie!!');
     return true;
   }
   return false;
