6006f2961fa10c10a49c8fd25862419f59048cc3
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-06-10 16:22:01 -0700

    sick wheelies abound

--- src/Motorcycle.js
+++ src/Motorcycle.js
@@ -1,6 +1,25 @@
 var Car = require('./Car.js');
 
 // Implement your Motorcycle here
-function Motorcycle() {}
+function Motorcycle(make, model, year, color, seats, passengers) {
+  Car.call(this, make, model, year, color, seats, passengers);
+}
+
+Motorcycle.prototype = Object.create(Car.prototype);
+Motorcycle.prototype.constructor = Motorcycle;
+
+
+Motorcycle.prototype.start = function() {
+  this.running = true;
+};
+
+Motorcycle.prototype.wheelie = function() {
+  if (this.running === true) {
+    console.log('Doing a sick wheelie!');
+  } else {
+    console.log('must be running to do a wheelie');
+  }
+};
 
 module.exports = Motorcycle;
+
