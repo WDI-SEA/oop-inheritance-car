2cb6eb1d9d3fbc23b76aefd616fdb9ec0fcc6968
Joe Marzullo <jmarzu13@gmail.com>
2016-06-10 16:40:40 -0700

    Boom

--- src/Motorcycle.js
+++ src/Motorcycle.js
@@ -2,7 +2,7 @@ var Car = require('./Car.js');
 
 // Implement your Motorcycle here
 function Motorcycle(make, model, year, color, seats, passengers) {
-  Car.call(this, make, model, year, color, seats, passengers);
+  Car.call(this, make, model, year, color, 2, passengers);
 }
 
 Motorcycle.prototype.start = function() {
@@ -15,8 +15,10 @@ Motorcycle.prototype.constructor = Motorcycle;
 Motorcycle.prototype.wheelie = function() {
   if (this.running) {
     console.log('doing a really sick sick wheelie');
+    return true;
   } else {
     console.log('car much be running');
+    return false;
   }
 };
 
