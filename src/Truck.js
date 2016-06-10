9f2f3df0b430c1299b3e0d31d92f8b69d567ee76
Joe Marzullo <jmarzu13@gmail.com>
2016-06-10 16:32:04 -0700

    All Done

--- src/Truck.js
+++ src/Truck.js
@@ -3,7 +3,7 @@ var Car = require('./Car.js');
 // Implement your Truck here
 
 function Truck(make, model, year, color, seats, passengers) {
-  Car.call(this, make, model, year, color, seats, passengers);
+  Car.call(this, make, model, year, color, 3, passengers);
 }
 
 Truck.prototype = Object.create(Car.prototype);
