3e767b8e8df2a388e096e1eb003ee21640c0bb9f
Ivar Lee <ivarr5@gmail.com>
2016-06-11 13:03:48 -0700

    reviewing the assignment as I was confused the first time around

--- src/Truck.js
+++ src/Truck.js
@@ -1,9 +1,12 @@
 var Car = require('./Car.js');
 
 // Implement your Truck here
+
+
 function Truck(make, model, year, color, seats, passengers) {
   Car.call(this, make, model, year, color, seats, passengers);
 }
+var truck = new Truck('Chevy', 'SilverRado', 2999, 'bleck', 3, ['Hunk Hail']);
 
 Truck.prototype = Object.create(Car.prototype);
 Truck.prototype.constructor = Truck;
