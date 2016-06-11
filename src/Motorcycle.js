d9688cc6dcd697160e47d9017f9c49d978feb2a0
Josh McBroom <mcbroomjc@gmail.com>
2016-06-11 00:10:41 -0700

    Pushing completed oop-inheritance-car deliverable

--- src/Motorcycle.js
+++ src/Motorcycle.js
@@ -1,6 +1,18 @@
 var Car = require('./Car.js');
 
 // Implement your Motorcycle here
-function Motorcycle() {}
+function Motorcycle(make, model, year, color, seats, passengers) {
+  Car.call(this, make, model, year, color, 2, passengers);
+}
+
+Car.prototype.wheelie = function() {
+  if (this.running) {
+    console.log('Doing a sick wheelie!!');
+    return true;
+  }
+  return false;
+};
+
+Motorcycle.prototype = Object.create(Car.prototype);
 
 module.exports = Motorcycle;
