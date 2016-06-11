90910012c619da5936d3a08d44035155a9ae985e
Ivar Lee <ivarr5@gmail.com>
2016-06-11 14:25:48 -0700

    changed to meet reqs

--- src/Motorcycle.js
+++ src/Motorcycle.js
@@ -4,14 +4,11 @@ var Car = require('./Car.js');
 function Motorcycle(make, model, year, color, passengers) {
   Car.call(this, make, model, year, color, 2, passengers);
 }
-Motorcycle.prototype.start = function() {
-  this.start = true;
-};
 
 Motorcycle.prototype = Object.create(Car.prototype);
 Motorcycle.prototype.constructor = Motorcycle;
 
-Motorcycle.prototype.wheelie = function() {
+Car.prototype.wheelie = function() {
   if (this.running) {
     console.log('Doing a wheelie that is sick');
     return true;
