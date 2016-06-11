37813ac8bc7e0e21544de12e5d74b59804db3a77
Ivar Lee <ivarr5@gmail.com>
2016-06-11 13:09:11 -0700

    fixed a lint issue

--- src/Truck.js
+++ src/Truck.js
@@ -6,7 +6,6 @@ var Car = require('./Car.js');
 function Truck(make, model, year, color, seats, passengers) {
   Car.call(this, make, model, year, color, seats, passengers);
 }
-var truck = new Truck('Chevy', 'SilverRado', 2999, 'bleck', 3, ['Hunk Hail']);
 
 Truck.prototype = Object.create(Car.prototype);
 Truck.prototype.constructor = Truck;
