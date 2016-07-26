3bacbd1da9da815447366b8a9d676bb1f68982fd
zaturnvalley <zacharycmason@gmail.com>
2016-07-26 16:55:21 -0700

    completed the truck test and code. onto motorcycle!

--- src/Motorcycle.js
+++ src/Motorcycle.js
@@ -3,8 +3,15 @@ var Car = require('./Car.js');
 // Implement your Motorcycle here
 function Motorcycle(make, model, year, color, passengers) {
   Car.call(this, make, model, year, color, passengers);
+  this.wheelie = function(){
+    if(!Car.running){
+      return false;
+    } else {
+      console.log("Doing a sick wheelie!!");
+    }
+  }
 }
 
-MotorCycle.prototype = Object.create(Car.prototype);
-MotorCycle.prototype.constructor = MotorCycle;
+Motorcycle.prototype = Object.create(Car.prototype);
+Motorcycle.prototype.constructor = Motorcycle;
 module.exports = Motorcycle;
--- test/motorcycleTest.js
+++ test/motorcycleTest.js
@@ -8,15 +8,16 @@ var Car = require('../src/Car');
 
 // Your tests will go below here
 console.log('Testing Motorcycle Constructor');
-var myMotorCycle = new MotorCycle('Honda', 'Goldwing', 1996, 'Black', 2, ['Zach', 'Elise']);
-assert.strictEqual(myMotorCycle instanceof MotorCycle, true, 'myMotorCycle is not an instanceof MotorCycle');
-assert.strictEqual(myMotorCycle instanceof Car, true, 'myMotorCycle is not an instance of Car');
-assert.strictEqual(myMotorCycle.make, 'Honda', 'Constructor did not set make.');
-assert.strictEqual(myMotorCycle.model, 'Goldwing', 'Constructor did not set model.');
-assert.strictEqual(myMotorCycle.year, 1996, 'Constructor did not set year.');
-assert.strictEqual(myMotorCycle.color, 'Black', 'Constructor did not set color of truck.');
-assert.deepEqual(myMotorCycle.passengers, ['Zach', 'Elise'], 'Constructor did not set number of passengers.');
-assert.strictEqual(myMotorCycle.seats, 2, 'Must default to 2 seats.');
-assert.strictEqual(myMotorCycle.running();, true)
-
+var myMotorcycle = new Motorcycle('Honda', 'Goldwing', 1996, 'Black', 2, ['Zach', 'Elise']);
+assert.strictEqual(myMotorcycle instanceof MotorCycle, true, 'myMotorcycle is not an instanceof MotorCycle');
+assert.strictEqual(myMotorcycle instanceof Car, true, 'myMotorcycle is not an instance of Car');
+assert.strictEqual(myMotorcycle.make, 'Honda', 'Constructor did not set make.');
+assert.strictEqual(myMotorcycle.model, 'Goldwing', 'Constructor did not set model.');
+assert.strictEqual(myMotorcycle.year, 1996, 'Constructor did not set year.');
+assert.strictEqual(myMotorcycle.color, 'Black', 'Constructor did not set color of truck.');
+assert.deepEqual(myMotorcycle.passengers, ['Zach', 'Elise'], 'Constructor did not set number of passengers.');
+assert.strictEqual(myMotorcycle.seats, 2, 'Must default to 2 seats.');
+assert.strictEqual(Car.running(), true, 'Must be running in order to pull of a sick wheelie');
+assert.strictEqual(myMotorcycle.wheelie();, true, 'Must be able to call wheelie function');
+assert.strictEqual(module.exports, Motorcycle, 'Must be attached to the Motorcycle Prototype');
 success();
