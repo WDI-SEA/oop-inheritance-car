ba3df961c66f0e9d5479feec4d6f50fca05b27eb
zmcdaniel <zmcdaniel@gmail.com>
2016-07-26 16:23:18 -0700

    Forgot to add testing for instances of Truck and Car to truckTest.js

--- test/truckTest.js
+++ test/truckTest.js
@@ -11,6 +11,8 @@ var Car = require('../src/Car');
 // test Truck constructor
 console.log('Testing Truck Constructor');
 var myTruck = new Truck('Chevrolet', 'Colorado', 2016, 'Silver', undefined, ['Becky']);
+assert.strictEqual(myTruck instanceof Truck, true, 'Not an instance of Truck!');
+assert.strictEqual(myTruck instanceof Car, true, 'Not an instance of Car');
 assert.strictEqual(myTruck.make, 'Chevrolet', 'Constructor didnt set make!');
 assert.strictEqual(myTruck.model, 'Colorado', 'Constructor didnt set mode!');
 assert.strictEqual(myTruck.year, 2016, 'Constructor didnt set year!');
