1bae9c41f19b560782a2a25f19c8a38c63ff283e
Boet Waldbjorn <boetw@me.com>
2016-07-26 16:29:18 -0700

    Truck and Motorcycle passes testing.

--- src/Motorcycle.js
+++ src/Motorcycle.js
@@ -1,6 +1,14 @@
 var Car = require('./Car.js');
 
 // Implement your Motorcycle here
-function Motorcycle() {}
+function Motorcycle(make, model, year, color, seats, passengers) {
+	Car.call(this, make, model, year, color, seats, passengers);
+	this.seats = 2;
+
+	this.running = false;
+}
+Motorcycle.prototype.wheelie = function() {
+  this.running = true;
+};
 
 module.exports = Motorcycle;
