d53c082755e51f89bb5035a2faa47d2149ba9006
kendradalley <kendracdalley@gmail.com>
2016-07-26 21:10:25 -0700

    completed Motorcycle and Truck with TDD tests

--- src/Motorcycle.js
+++ src/Motorcycle.js
@@ -1,6 +1,24 @@
 var Car = require('./Car.js');
 
-// Implement your Motorcycle here
-function Motorcycle() {}
+// new constructor & parameters
+function Motorcycle(make, model, year, color, seats, passengers) {
+  Car.call(this, make, model, year, color, 2, passengers);
+}
+
+//constructor inherits Car
+Motorcycle.prototype = Object.create(Car.prototype);
+Motorcycle.prototype.constructor = Motorcycle;
+
+Motorcycle.prototype.wheelie = function(){
+  if(this.running === true){
+    console.log("Doing a sick wheelie!!");
+    return true;
+  }
+  return false;
+}
+
+//declare instances
+// var motorcycle = new Motorcycle();
+// var car = new Car();
 
 module.exports = Motorcycle;
--- src/Truck.js
+++ src/Truck.js
@@ -1,6 +1,19 @@
 var Car = require('./Car.js');
 
 // Implement your Truck here
-function Truck() {}
+function Truck(make, model, year, color, seats, passengers) { //this needs to be consistent with order of Car declarations
+  //constructor parameters- since they are already constructed in Car, call them in
+  Car.call(this, make, model, year, color, 3, passengers);
+}
+
+//Truck inherits from Car
+Truck.prototype = Object.create(Car.prototype);
+Truck.prototype.constructor = Truck;
+
+//Instance of truck and car
+var truck = new Truck();
+var car = new Car();
 
 module.exports = Truck;
+
+
