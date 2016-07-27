73d283b27c9ad5428500fccf6f588d8b2d185aa5
ChrisKenyon79 <ckenyon79@gmail.com>
2016-07-27 08:36:53 -0700

    missing wheelie test

--- src/Truck.js
+++ src/Truck.js
@@ -1,6 +1,9 @@
 var Car = require('./Car.js');
 
 // Implement your Truck here
-function Truck() {}
+function Truck(make, model, year, color, seats, passengers) {
+  Car.call(this);
+  this.seats = 3;
+}
 
 module.exports = Truck;
