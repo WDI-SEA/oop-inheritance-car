8be87c8dd4770ca732a01dacc770ac4bfb52f339
kedeger <kedeger@uw.edu>
2016-07-26 17:19:24 -0700

    Tests created, popping wheelies

--- test/motorcycleTest.js
+++ test/motorcycleTest.js
@@ -7,3 +7,18 @@ var Motorcycle = require('../src/Motorcycle');
 var Car = require('../src/Car');
 
 // Your tests will go below here
+console.log('Testing Constructor');
+var myMotorcycle = new Motorcycle('Ducati', '1098s', 2007, 'Red', 2);
+assert.strictEqual(myMotorcycle.make, 'Ducati', 'Constructor did not set make.');
+assert.strictEqual(myMotorcycle.model, '1098s', 'Constructor did not set model.');
+assert.strictEqual(myMotorcycle.year, 2007, 'Constructor did not set year.');
+assert.strictEqual(myMotorcycle.color, 'Red', 'Constructor did not set color.');
+assert.strictEqual(myMotorcycle.seats, 2, 'Constructor did not set seats.');
+success();
+
+
+console.log('Testing wheelie');
+assert.equal(typeof(myMotorcycle.wheelie), 'function', 'there is no wheelie function');
+myMotorcycle.start();
+assert.strictEqual(myMotorcycle.wheelie(), true, 'Failed to pop a wheelie');
+success();
--- test/truckTest.js
+++ test/truckTest.js
@@ -7,3 +7,11 @@ var Truck = require('../src/Truck');
 var Car = require('../src/Car');
 
 // Your tests will go below here
+console.log('Testing Constructor');
+var myTruck = new Truck('Chevy', 'Silverado', 2013, 'Red', 4);
+assert.strictEqual(myTruck.make, 'Chevy', 'Constructor did not set make.');
+assert.strictEqual(myTruck.model, 'Silverado', 'Constructor did not set model.');
+assert.strictEqual(myTruck.year, 2013, 'Constructor did not set year.');
+assert.strictEqual(myTruck.color, 'Red', 'Constructor did not set color.');
+assert.strictEqual(myTruck.seats, 4, 'Constructor did not set seats.');
+success();
