1d4ef2cb7762d28e0dd5b08e04292d69ac027f32
miguelapou <Retrofuture@Miguels-MacBook-Pro.local>
2016-07-26 22:38:53 -0700

    still working on the end of phase 2 onward

--- src/Motorcycle.js
+++ src/Motorcycle.js
@@ -1,6 +1,12 @@
 var Car = require('./Car.js');
 
 // Implement your Motorcycle here
-function Motorcycle() {}
+function Motorcycle(make, model, year, color, passengers) {
+  Car.call(this, make, model, year, color, passengers);
+  this.seats = 2;
+}
+
+Motorcycle.prototype = Object.create(Car.prototype);
+Motorcycle.prototype.constructor = Motorcycle;
 
 module.exports = Motorcycle;
