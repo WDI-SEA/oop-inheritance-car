bf747fd7ad9e22b3e909e2baee33c4eba59315bd
shway250 <jackson_duhon@yahoo.com>
2016-07-26 17:18:16 -0700

    wrote and passed all tests for motorcycle and truck

--- src/Motorcycle.js
+++ src/Motorcycle.js
@@ -1,6 +1,25 @@
 var Car = require('./Car.js');
 
 // Implement your Motorcycle here
-function Motorcycle() {}
+function Motorcycle(make, model, year, color, seats, passengers) {
+  Car.call(this, make, model, year, color, seats, passengers);
+  this.seats = seats || 2;
+  this.running = false;
+}
+
+Motorcycle.prototype = Object.create(Car.prototype);
+Motorcycle.prototype.constructor = Car;
+
+Motorcycle.prototype.turnOn = function(){
+  this.running = true;
+}
+
+Motorcycle.prototype.wheelie = function(){
+  if(this.running){
+    console.log("Doing a sick wheelie!!");
+    return true;
+  }
+  return false;
+}
 
 module.exports = Motorcycle;
