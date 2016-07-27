48bca5651547d3eb7f96f0c7446780cd974d4f10
teemopower <jimmykim925@gmail.com>
2016-07-26 20:59:32 -0700

    updated solution

--- src/Motorcycle.js
+++ src/Motorcycle.js
@@ -1,6 +1,27 @@
+
 var Car = require('./Car.js');
 
-// Implement your Motorcycle here
-function Motorcycle() {}
+function Motorcycle(make, model, year, color, seats, passengers, running){
+  Car.call(this, make, model, year, color, passengers);
+
+  this.seats = seats || 2;
+  this.running = false;  
+}
+
+//create relationship
+Motorcycle.prototype = Object.create(Car.prototype);
+Motorcycle.prototype.constructor = Motorcycle;
+
+Motorcycle.prototype.wheelie = function(){
+  if(this.running){
+    console.log("Doing a sick wheelie!!");
+    return true;
+  }
+  return false;
+}
+
+Motorcycle.prototype.start = function() {
+  this.running = true;
+};
 
 module.exports = Motorcycle;
--- test/truckTest.js
+++ test/truckTest.js
@@ -6,4 +6,13 @@ var success = require('./helpers/success');
 var Truck = require('../src/Truck');
 var Car = require('../src/Car');
 
-// Your tests will go below here
+// test constructor
+console.log('Testing Constructor');
+var myTruck = new Truck('Ford', 'f150', 1999, 'Red', 2);
+assert.strictEqual(myTruck.make, 'Ford', 'Constructor did not set make.');
+assert.strictEqual(myTruck.model, 'f150', 'Constructor did not set model.');
+assert.strictEqual(myTruck.year, 1999, 'Constructor did not set year.');
+assert.strictEqual(myTruck.color, 'Red', 'Constructor did not set color.');
+assert.strictEqual(myTruck.seats, 2, 'Constructor did not set seats.');
+
+success('TRUCK TEST success');
