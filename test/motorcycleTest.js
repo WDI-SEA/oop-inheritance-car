c76b76b4b80d5d5b3a70aafcd5e00fd83be0d21a
britbroet <brittanybroetje@gmail.com>
2016-10-03 13:02:24 -0700

    brit oop truck and motorcycle

--- test/motorcycleTest.js
+++ test/motorcycleTest.js
@@ -7,3 +7,30 @@ var Motorcycle = require('../src/Motorcycle');
 var Car = require('../src/Car');
 
 // Your tests will go below here
+
+//constructor tests
+
+console.log('Testing Motorcycle Constructor');
+
+var myMotorcycle = new Motorcycle('Harley', 'Roadster', 1903, 'Black', null, ['batman', 'yogi']);
+
+assert.strictEqual(myMotorcycle instanceof Motorcycle, true, 'myMotorcycle not instance of Car');
+assert.strictEqual(myMotorcycle instanceof Car, true, 'myMotorcycle not instance of Car');
+assert.strictEqual(myMotorcycle.make, 'Harley', 'Constructor did not set make.');
+assert.strictEqual(myMotorcycle.model, 'Roadster', 'Constructor did not set model.');
+assert.strictEqual(myMotorcycle.year, 1903, 'Constructor did not set year.');
+assert.strictEqual(myMotorcycle.color, 'Black', 'Constructor did not set color.');
+assert.strictEqual(myMotorcycle.seats, 2, 'Constructor did not set seats to 3.');
+assert.deepEqual(myMotorcycle.passengers, ['batman', 'yogi'], 'Constructor did not set passengers.');
+success();
+
+
+// test Wheelie
+
+console.log('Testing the wheelie');
+assert.equal(typeof(myMotorcycle.wheelie), 'function', 'there is no wheelie function');
+myMotorcycle.off();
+assert.strictEqual(myMotorcycle.wheelie(), false, 'Motorcycle allowed to do wheelie while not running.');
+myMotorcycle.start();
+assert.strictEqual(myMotorcycle.wheelie(), true, 'Motorcycle not allowed to do wheelie while running.');
+success();
--- test/truckTest.js
+++ test/truckTest.js
@@ -7,3 +7,21 @@ var Truck = require('../src/Truck');
 var Car = require('../src/Car');
 
 // Your tests will go below here
+
+// test constructor
+console.log('Testing Truck Constructor');
+
+var myTruck = new Truck('Toyota', 'Tacoma', 1999, 'Blue', null, ['batman', 'yogi']);
+
+assert.strictEqual(myTruck instanceof Truck, true, 'myTruck not instance of Truck');
+assert.strictEqual(myTruck instanceof Car, true, 'myTruck not instance of Car');
+assert.strictEqual(myTruck.make, 'Toyota', 'Constructor did not set make.');
+assert.strictEqual(myTruck.model, 'Tacoma', 'Constructor did not set model.');
+assert.strictEqual(myTruck.year, 1999, 'Constructor did not set year.');
+assert.strictEqual(myTruck.color, 'Blue', 'Constructor did not set color.');
+assert.strictEqual(myTruck.seats, 3, 'Constructor did not set seats to 3.');
+assert.deepEqual(myTruck.passengers, ['batman', 'yogi'], 'Constructor did not set passengers.');
+success();
+
+
+
