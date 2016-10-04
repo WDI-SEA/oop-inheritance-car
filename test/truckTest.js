ab0e73f221e2b3a3b1be5921fc609c666e0dbb46
jedimasterclos <carlossrinivas@gmail.com>
2016-10-03 22:32:20 -0700

    Carlos: Truck & Bike OOP

--- test/truckTest.js
+++ test/truckTest.js
@@ -7,3 +7,15 @@ var Truck = require('../src/Truck');
 var Car = require('../src/Car');
 
 // Your tests will go below here
+console.log("Testing truck constructor")
+
+var myTruck = new Truck('Toyota', 'Tundra', 2012, 'Silver', 6);
+
+assert.strictEqual(myTruck instanceof Car, true, "Not an instance of a car");
+assert.strictEqual(myTruck instanceof Truck, true, "Not an instance of a truck");
+assert.strictEqual(myTruck.make, 'Toyota', 'Constructor did not set make.');
+assert.strictEqual(myTruck.model, 'Tundra', 'Constructor did not set model.');
+assert.strictEqual(myTruck.year, 2012, 'Constructor did not set year.');
+assert.strictEqual(myTruck.color, 'Silver', 'Constructor did not set color.');
+assert.strictEqual(myTruck.seats, 6, 'Constructor did not set seats.');
+success('Truck good');
