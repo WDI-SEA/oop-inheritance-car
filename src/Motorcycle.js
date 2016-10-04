0d8db1e599a1d16f016356e08b10b715657d6c66
jamiebb <jamieandburton@gmail.com>
2016-10-04 09:02:55 -0700

    Deliverable

--- src/Motorcycle.js
+++ src/Motorcycle.js
@@ -1,6 +1,28 @@
 var Car = require('./Car.js');
 
 // Implement your Motorcycle here
-function Motorcycle() {}
+function Motorcycle(make, model, year, color, seats, passengers, running) {
+	
+	// Car.call(make, model, year, color, seats, passengers);
+
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.color = color;
+	this.seats = 2;
+	this.passengers = passengers;
+	this.running = running;
+}
+
+// Motorcycle.prototype = Object.create(Car.prototype);
+// Motorcycle.prototype.constructor = Motorcycle.prototype;
+
+Motorcycle.prototype.wheelie = function() {
+	if (this.running) {
+		console.log("Doing a sick wheelie!");
+	} else {
+		return false;
+	}
+}
 
 module.exports = Motorcycle;
