0f37343319e381b64e239c8d40fc969e14404819
BertHaine <bertram.haine@gmail.com>
2016-10-03 22:58:43 -0700

    Finished Deliverable

--- test/motorcycleTest.js
+++ test/motorcycleTest.js
@@ -7,3 +7,14 @@ var Motorcycle = require('../src/Motorcycle');
 var Car = require('../src/Car');
 
 // Your tests will go below here
+console.log('Testing Motorcycle');
+var myMotorcycle = new Motorcycle('Harley', 'Davidson', 1989, 'Black', 2);
+assert.strictEqual(myMotorcycle.make, 'Harley', 'Constructor did not set make.');
+assert.strictEqual(myMotorcycle.model, 'Davidson', 'Constructor did not set model.');
+assert.strictEqual(myMotorcycle.year, 1989, 'Constructor did not set year.');
+assert.strictEqual(myMotorcycle.color, 'Black', 'Constructor did not set color.');
+assert.strictEqual(myMotorcycle.seats, 2, 'Constructor did not set seats.');
+
+console.log('Testing wheelie');
+assert.strictEqual(myMotorcycle.wheelie(), true, 'Motorcycle did not do a sick wheelie');
+success();
