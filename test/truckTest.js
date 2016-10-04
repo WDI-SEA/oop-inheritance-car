a900c549cab29115cc9145d8dd1265e33bacff60
alfordsiler <alfordsiler@gmail.com>
2016-10-03 20:01:40 -0700

    inheritance car complete

--- test/truckTest.js
+++ test/truckTest.js
@@ -7,3 +7,14 @@ var Truck = require('../src/Truck');
 var Car = require('../src/Car');
 
 // Your tests will go below here
+console.log('Testing Truck Constructor');
+
+var myTruck = new Truck('Ford', 'F-150', 2016, 'Black')
+assert.strictEqual(myTruck instanceof Car, true, "Not an instance of Car");
+assert.strictEqual(myTruck instanceof Truck, true, "Not an instance of Truck");
+assert.strictEqual(myTruck.make, 'Ford', 'Constructor did not set make.');
+assert.strictEqual(myTruck.model, 'F-150', 'Constructor did not set model.');
+assert.strictEqual(myTruck.year, 2016, 'Constructor did not set year.');
+assert.strictEqual(myTruck.color, 'Black', 'Constructor did not set color.');
+assert.strictEqual(myTruck.seats, 3, 'Constructor did not set seats.');
+success('Your Truck is Ready. Now move on to Motorcycle.');
