620fdc834ca104521c80a1f76c082f90afe68e8b
tannerkovach <kovach.tanner@gmail.com>
2016-10-04 09:09:36 -0700

    deliverable

--- test/truckTest.js
+++ test/truckTest.js
@@ -7,3 +7,27 @@ var Truck = require('../src/Truck');
 var Car = require('../src/Car');
 
 // Your tests will go below here
+
+
+var myTruck = new Truck("Chevy", "Colorado", 2016, "Red", 3);
+
+assert.deepEqual(myTruck instanceof Car, true, "Truck is not an instance of Car");
+assert.strictEqual(myTruck instanceof Truck, true, "Truck is not an instance of Truck");
+
+assert.strictEqual(myTruck.make, 'Chevy', 'Constructor did not set make.');
+assert.strictEqual(myTruck.model, 'Colorado', 'Constructor did not set model.');
+assert.strictEqual(myTruck.year, 2016, 'Constructor did not set year.');
+assert.strictEqual(myTruck.color, 'Red', 'Constructor did not set color.');
+assert.strictEqual(myTruck.seats, 3, 'Constructor did not set seats.');
+success();
+
+
+
+
+
+
+
+
+
+
+
